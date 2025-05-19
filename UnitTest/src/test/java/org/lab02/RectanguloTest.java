8fa9cf913e364d5ceaac069ed0d5cae74a229015
MendozaEmerson <emendozahi@unsa.edu.pe>
2025-05-19 11:18:16 -0500

    V1.1.0-Se arreglaron los problemas de redondeo e hizo test con valores 0 en ambos parametros

--- UnitTest/src/test/java/org/lab02/RectanguloTest.java
+++ UnitTest/src/test/java/org/lab02/RectanguloTest.java
@@ -15,23 +15,42 @@ public class RectanguloTest {
     @Test
     void testCalculoCorrecto() {
         assertThat(Rectangulo.calcularArea(4, 2), is(8.0));
-        assertThat(Rectangulo.calcularArea(12, 5.6), is(67.2));
-        assertThat(Rectangulo.calcularArea(24.9, 19), is(473.1));
+        assertThat(Rectangulo.calcularArea(12, 5.6), closeTo(67.2,0.01));
+        assertThat(Rectangulo.calcularArea(24.9, 19), closeTo(473.1,0.01));
         assertThat(Rectangulo.calcularArea(120.9, 234.2), is(28314.78));
+    }
+
+    @DisplayName("Altura con valor de cero y base entera positiva")
+    @Test
+    void testAlturaCero() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            Rectangulo.calcularArea(45, 0);
+        });
+        assertThat(exception.getMessage(), is("Base y altura deben ser mayores que cero."));
+    }
+
+    @DisplayName("Base con valor de cero y altura decimal positiva")
+    @Test
+    void testBaseCero() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            Rectangulo.calcularArea(0, 23.9);
+        });
+        assertThat(exception.getMessage(), is("Base y altura deben ser mayores que cero."));
+    }
+
+    @DisplayName("Base y altura con valores cero")
+    @Test
+    void testBaseAlturaCero() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            Rectangulo.calcularArea(0, 0);
+        });
+        assertThat(exception.getMessage(), is("Base y altura deben ser mayores que cero."));
     }
 
 //    @Test
 //    void testBaseNegativa() {
 //        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
 //            Rectangulo.calcularArea(-1, 5);
-//        });
-//        assertEquals("Base y altura deben ser mayores que cero.", exception.getMessage());
-//    }
-//
-//    @Test
-//    void testAlturaCero() {
-//        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-//            Rectangulo.calcularArea(5, 0);
 //        });
 //        assertEquals("Base y altura deben ser mayores que cero.", exception.getMessage());
 //    }
